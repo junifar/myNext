4ba68bfe14ef62189a9c38f934b4955cda1f8c90
Junifar Hidayat <junifar.hidayat@dhanapala.id>
2021-08-21 21:17:25 +0700

    Hapus Test

--- components/topbar/index.js
+++ components/topbar/index.js
@@ -19,7 +19,7 @@ const TopBar = (props) => {
     <AppBar position="fixed">
       <Toolbar>
         <Typography variant="h6" className={classes.title}>
-          {props.title} test
+          {props.title}
         </Typography>
       </Toolbar>
     </AppBar>
