eb32661ecfe25084bb3273eb3299ad49597e32a3
junifarhidayat <junifar@gmail.com>
2021-08-28 15:37:34 +0700

    Adding navbar mobile

--- components/common/m/navbar/index.js
+++ components/common/m/navbar/index.js
@@ -1,6 +1,35 @@
+import {
+  AppBar,
+  Box,
+  IconButton,
+  Toolbar,
+  Typography,
+} from "@material-ui/core";
+import MenuIcon from "@material-ui/icons/Menu";
 import { Fragment } from "react";
 
 const MNavbar = (props) => {
-  return <Fragment>{props.title}</Fragment>;
+  return (
+    <Fragment>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}>
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              {props.title}
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </Box>
+      {props.title}
+    </Fragment>
+  );
 };
 export default MNavbar;
